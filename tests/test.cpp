6316e395d4bb6db1fcc2818cac5f03d80dba5322
J. Cian Capacci <cian.capacci@gmail.com>
2025-03-03 22:57:26 -0500

    Added basic file parsing tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,5 +4,27 @@
 #include "computron.h"
 
 TEST_CASE("File Processing Tests", "[CompuTron]") {
+	std::array<int, memorySize> memory{0};
+	int accumulator{0};
+	size_t instructionCounter{0};
+	int instructionRegister{0};
+	size_t operationCode{0};
+	size_t operand{0};
 
+	//Attempt to ingest an existing file properly
+	load_from_file(memory, "p1.txt");
+	REQUIRE(memory[2] == 2007);
+
+	//Attempt to ingest an empty file
+	std::array<int, memorySize> emptyReadTestArray{0};
+	load_from_file(memory, "empty.txt");
+	REQUIRE(emptyReadTestArray[0] == 0);
+	//REQUIRE_THROWS_AS(load_from_file(memory, "empty.txt"), std::runtime_error);
+	
+	//Attempt to ingest a nonexistent file
+	REQUIRE_THROWS_AS(load_from_file(memory, "nonexistent.txt"), std::runtime_error);
+}
+
+TEST_CASE("Basic Command Parsing Tests", "[CompuTron]") {
+	
 }
