512891ec0c996db2ba1b075d3c14e481d9c378fa
J. Cian Capacci <cian.capacci@gmail.com>
2025-03-03 20:51:47 -0500

    Removed extraneous extra case from opcode checker

--- src/computron.cpp
+++ src/computron.cpp
@@ -49,8 +49,7 @@ Command opCodeToCommand(size_t opCode) {
 		case 41: return Command::branchNeg;
 		case 42: return Command::branchZero;
 		case 43: return Command::halt;
-		default: 
-			throw std::runtime_error("A command was run with an invalid OpCode!");
+		//default: throw std::runtime_error("A command was run with an invalid OpCode!");
 	};
 }
 
